6ae0c0717c03dc4c7865c2895a91229c5d4e251e
Jewoc_DEV <jerome.legousse@gmail.com>
2025-09-17 22:30:50 +0200

    Delay Query Monitor integration registration

--- sitepulse_FR/includes/integrations.php
+++ sitepulse_FR/includes/integrations.php
@@ -2,18 +2,28 @@
 if (!defined('ABSPATH')) exit;
 
 // Query Monitor integration
-if (class_exists('QueryMonitor')) {
-    class SitePulse_QM_Collector extends QM_Collector {
-        public $id = 'sitepulse';
-        public function name() { return 'SitePulse'; }
-        public function process() {
-            $this->data['load_time'] = get_option('sitepulse_last_load_time', 'N/A');
-            $this->data['uptime'] = get_option('sitepulse_uptime_log', []);
+add_action('plugins_loaded', function () {
+    if (!class_exists('QM_Collector')) {
+        return;
+    }
+
+    if (!class_exists('SitePulse_QM_Collector', false)) {
+        class SitePulse_QM_Collector extends QM_Collector {
+            public $id = 'sitepulse';
+
+            public function name() { return 'SitePulse'; }
+
+            public function process() {
+                $this->data['load_time'] = get_option('sitepulse_last_load_time', 'N/A');
+                $this->data['uptime'] = get_option('sitepulse_uptime_log', []);
+            }
         }
     }
-    add_filter('qm/collectors', function($collectors) {
+
+    add_filter('qm/collectors', function ($collectors) {
         $collectors['sitepulse'] = new SitePulse_QM_Collector();
         return $collectors;
     });
+
     sitepulse_log('Integrated with Query Monitor');
-}
+}, 100);
