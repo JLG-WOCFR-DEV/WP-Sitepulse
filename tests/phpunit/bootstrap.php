85191cb1112c6bf16a3228a624fb4b9271e7fb9c
Jewoc_DEV <jerome.legousse@gmail.com>
2025-09-26 15:53:07 +0200

    Add admin settings cleanup tests

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -6,17 +6,14 @@
 $_tests_dir = getenv('WP_TESTS_DIR');
 
 if (!$_tests_dir) {
-    $_tests_dir = rtrim(sys_get_temp_dir(), '/\') . '/wordpress-tests-lib';
+    $_tests_dir = rtrim(sys_get_temp_dir(), "/\\") . '/wordpress-tests-lib';
 }
 
 if (!file_exists($_tests_dir . '/includes/functions.php')) {
-    fwrite(STDERR, "Could not find WordPress tests in ${_tests_dir}.\n");
+    fwrite(STDERR, sprintf("Could not find WordPress tests in %s.\n", $_tests_dir));
     exit(1);
 }
 
 require_once $_tests_dir . '/includes/functions.php';
-
 require_once __DIR__ . '/includes/stubs.php';
-
 require $_tests_dir . '/includes/bootstrap.php';
-
